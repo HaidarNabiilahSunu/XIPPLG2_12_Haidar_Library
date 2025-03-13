afaaf433ae7197d992bb9a7d3ed5e2978a56492c
HaidarNabiilahSunu <541231089@student.smktelkom-pwt.sch.id>
2025-03-13 11:45:06 +0700

    Laravel Library - Restful API [ Book ]

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoryController; // Pastikan ini menggunakan huruf kapital pada "C"
+use App\Http\Controllers\CategoryController;  // Pastikan ini menggunakan huruf kapital pada "C"
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\UserController;
-
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\LoanController;
+use App\Http\Controllers\ReviewController;
 
 // Menggunakan apiResource untuk resource CRUD standar
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('books', BookController::class);
 Route::apiResource('users', UserController::class);
 
-// Route contoh untuk mengambil user yang sudah autentikasi menggunakan Sanctum
+// Route contoh untuk mengambil user yang sudah autentikasi menggunakan Sanctum
